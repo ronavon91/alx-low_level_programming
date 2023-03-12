0e86c8224cd4d088394afd61b55233d07555beac
“ronavon91” <ronaldadjivon@yahoo.com>
2023-03-12 05:34:24 -0700

    task 1

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,14 +1,12 @@
 #include "main.h"
+
 /**
- * _isupper - uppercase letters
- * @c: char to check
- *
- * Return: 0 or 1
+ * _isupper - check for upper case letter
+ * @c : character to check the letter case
+ * Return:0 or 1
  */
+
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-	return (0);
+        return (c >= 65 && c <= 90);
 }
